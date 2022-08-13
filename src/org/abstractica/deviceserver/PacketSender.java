c7cf08cb382ae35a45744fb591aa5ead235a1510
Tobias Grundtvig <tgrundtvig@gmail.com>
2022-08-13 02:28:49 +0200

    Removed unused interfaces

--- src/org/abstractica/deviceserver/PacketSender.java
+++
@@ -1,9 +0,0 @@
-package org.abstractica.deviceserver;
-
-public interface PacketSender
-{
-    int sendPacket(   DevicePacket packet,
-                      boolean blocking,
-                      boolean forceSend,
-                      PacketSendCallback callback   ) throws InterruptedException;
-}
--- src/org/abstractica/deviceserver/SendInfo.java
+++
@@ -1,9 +0,0 @@
-package org.abstractica.deviceserver;
-
-public interface SendInfo
-{
-    public boolean isBlocking();
-    public boolean forceSend();
-    public DevicePacket getPacket();
-    public PacketSendCallback getCallback();
-}
